cd8b43ddacb1e031783c457263c6178c1a567145
GadzaG <paperka.lip@gmail.com>
2024-03-08 19:18:38 +0300

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,123 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+	<button type="button" class="btn btn-default">Add</button>
+	<table class="table">
+		<thead>
+			<tr>
+				<th scope="col">#</th>
+				<th scope="col">Task name</th>
+				<th scope="col">Status</th>
+				<th scope="col">Edit</th>
+				<th scope="col">Remove</th>
+			</tr>
+		</thead>
+		<tbody>
+			<task v-for="(task, index) in tasks" :key="task.id"></task>
+		</tbody>
+	</table>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script></script>
 
-nav {
-  padding: 30px;
-}
+<style></style>
 
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+<!-- <div class="header">
+		<h1>TODO List Demo App</h1>
+		<label>Do it now.</label>
+	</div>
+	<div class="table">
+		<button type="button" class="btn btn-default">Add</button>
+		<table class="table">
+			<thead>
+				<tr>
+					<th scope="col">#</th>
+					<th scope="col">Task name</th>
+					<th scope="col">Status</th>
+					<th scope="col">Edit</th>
+					<th scope="col">Remove</th>
+				</tr>
+			</thead>
+			<tbody>
+				
+				<tr>
+					<th scope="row">25</th>
+					<td>Send Email</td>
+					<td>
+						<button type="button" class="btn btn-default">In Progress</button>
+					</td>
+					<td>
+						<button type="button" class="btn btn-default">
+							<span
+								class="glyphicon glyphicon-pencil"
+								aria-hidden="true"
+							></span>
+						</button>
+					</td>
+					<td>
+						<button type="button" class="btn btn-default">
+							<span class="glyphicon glyphicon-trash" aria-hidden="true"></span>
+						</button>
+					</td>
+				</tr>
+				<tr>
+					<th scope="row">24</th>
+					<td>Finish Assignment</td>
+					<td>
+						<button type="button" class="btn btn-default">Complete</button>
+					</td>
+					<td>
+						<button type="button" class="btn btn-default">
+							<span
+								class="glyphicon glyphicon-pencil"
+								aria-hidden="true"
+							></span>
+						</button>
+					</td>
+					<td>
+						<button type="button" class="btn btn-default">
+							<span class="glyphicon glyphicon-trash" aria-hidden="true"></span>
+						</button>
+					</td>
+				</tr>
+				<tr>
+					<th scope="row">24</th>
+					<td>Bake Cake</td>
+					<td>
+						<button type="button" class="btn btn-default">Todo</button>
+					</td>
+					<td>
+						<button type="button" class="btn btn-default">
+							<span
+								class="glyphicon glyphicon-pencil"
+								aria-hidden="true"
+							></span>
+						</button>
+					</td>
+					<td>
+						<button type="button" class="btn btn-default">
+							<span class="glyphicon glyphicon-trash" aria-hidden="true"></span>
+						</button>
+					</td>
+				</tr>
+				<tr>
+					<th scope="row">24</th>
+					<td>Write Blog Post</td>
+					<td>
+						<button type="button" class="btn btn-default">In Progress</button>
+					</td>
+					<td>
+						<button type="button" class="btn btn-default">
+							<span
+								class="glyphicon glyphicon-pencil"
+								aria-hidden="true"
+							></span>
+						</button>
+					</td>
+					<td>
+						<button type="button" class="btn btn-default">
+							<span class="glyphicon glyphicon-trash" aria-hidden="true"></span>
+						</button>
+					</td>
+				</tr>
+			</tbody>
+		</table>
+	</div> -->
---
+++ src/components/Taks.vue
@@ -0,0 +1,25 @@
+<template>
+	<td>
+		<tr>
+			<th scope="row">25</th>
+			<td></td>
+			<td>
+				<button type="button" class="btn btn-default"></button>
+			</td>
+			<td>
+				<button type="button" class="btn btn-default">
+					<span class="glyphicon glyphicon-pencil" aria-hidden="true"></span>
+				</button>
+			</td>
+			<td>
+				<button type="button" class="btn btn-default">
+					<span class="glyphicon glyphicon-trash" aria-hidden="true"></span>
+				</button>
+			</td>
+		</tr>
+	</td>
+</template>
+
+<script></script>
+
+<style></style>
